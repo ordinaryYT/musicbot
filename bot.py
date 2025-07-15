9b5c151991dddc1baa225c5d056a610fc97d2d9e
ordinaryYT <baileybailey698@gmail.com>
2025-07-15 09:37:55 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,87 @@
+import discord
+from discord.ext import commands
+import youtube_dl
+import asyncio
+
+# Bot setup
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+# Suppress noise about console usage from youtube-dl
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'  # IPv4
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        
+        if 'entries' in data:
+            data = data['entries'][0]
+            
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="!help"))
+
+@bot.command()
+async def play(ctx, *, url):
+    """Plays from a url (almost anything youtube_dl supports)"""
+    
+    if not ctx.message.author.voice:
+        await ctx.send("You are not connected to a voice channel")
+        return
+    
+    channel = ctx.message.author.voice.channel
+    
+    if ctx.voice_client:
+        await ctx.voice_client.move_to(channel)
+    else:
+        voice_client = await channel.connect()
+    
+    async with ctx.typing():
+        player = await YTDLSource.from_url(url, loop=bot.loop, stream=True)
+        ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
+    
+    await ctx.send(f'Now playing: {player.title}')
+
+@bot.command()
+async def stop(ctx):
+    """Stops and disconnects the bot from voice"""
+    
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+        await ctx.send("Disconnected")
+    else:
+        await ctx.send("I'm not connected to a voice channel")
+
+# Run the bot
+bot.run(os.getenv('DISCORD_TOKEN'))
