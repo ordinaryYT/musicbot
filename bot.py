5c87a541ae0b08c11d0aecc2c5c6b95d6b7335b7
ordinaryYT <baileybailey698@gmail.com>
2025-07-15 10:10:46 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,94 +1,77 @@
+import os
+import asyncio
 import discord
 from discord.ext import commands
-import youtube_dl
-import asyncio
-import os
+from yt_dlp import YoutubeDL
 from dotenv import load_dotenv
 
-# Load environment variables from .env file (for local testing)
+# Load environment variables
 load_dotenv()
 
-# Bot setup
+# Voice stability workarounds
+discord.opus.load_opus('libopus.so.0')
 intents = discord.Intents.default()
 intents.message_content = True
+
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Suppress noise about console usage from youtube-dl
-youtube_dl.utils.bug_reports_message = lambda: ''
-
-ytdl_format_options = {
+# Optimized YTDL configuration
+ytdl_format = {
     'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
+    'extractaudio': True,
+    'audioformat': 'mp3',
     'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
     'quiet': True,
     'no_warnings': True,
     'default_search': 'auto',
-    'source_address': '0.0.0.0'  # IPv4
+    'source_address': '0.0.0.0'
 }
 
-ffmpeg_options = {
-    'options': '-vn'
-}
+ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+class VoiceConnectionError(commands.CommandError):
+    pass
 
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-        self.data = data
-        self.title = data.get('title')
-        self.url = data.get('url')
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="your commands"))
 
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+async def ensure_voice(ctx):
+    if not ctx.author.voice:
+        raise VoiceConnectionError("You need to be in a voice channel.")
+    
+    if ctx.voice_client:
+        if ctx.voice_client.channel != ctx.author.voice.channel:
+            await ctx.voice_client.move_to(ctx.author.voice.channel)
+    else:
+        await ctx.author.voice.channel.connect()
+
+@bot.command()
+async def play(ctx, *, query):
+    """Plays audio from YouTube"""
+    try:
+        await ensure_voice(ctx)
+        
+        ytdl = YoutubeDL(ytdl_format)
+        loop = asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(query, download=False))
         
         if 'entries' in data:
             data = data['entries'][0]
             
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
-
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name} (ID: {bot.user.id})')
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="!help"))
-
-@bot.command()
-async def play(ctx, *, url):
-    """Plays from a URL (YouTube, SoundCloud, etc.)"""
-    
-    if not ctx.author.voice:
-        await ctx.send("You're not in a voice channel!")
-        return
-    
-    channel = ctx.author.voice.channel
-    
-    if ctx.voice_client:
-        await ctx.voice_client.move_to(channel)
-    else:
-        await channel.connect()
-    
-    async with ctx.typing():
-        player = await YTDLSource.from_url(url, loop=bot.loop, stream=True)
-        ctx.voice_client.play(player, after=lambda e: print(f'Error: {e}') if e else None)
-    
-    await ctx.send(f'Now playing: {player.title}')
+        audio_source = discord.FFmpegPCMAudio(data['url'], **ffmpeg_options)
+        ctx.voice_client.play(audio_source)
+        
+        await ctx.send(f"Now playing: {data['title']}")
+    except Exception as e:
+        await ctx.send(f"Error: {str(e)}")
 
 @bot.command()
 async def stop(ctx):
-    """Stops playback and leaves voice channel"""
-    
+    """Stops playback and leaves voice"""
     if ctx.voice_client:
         await ctx.voice_client.disconnect()
-        await ctx.send("Disconnected")
-    else:
-        await ctx.send("I'm not in a voice channel!")
+    await ctx.send("Disconnected")
 
-# Run the bot
 bot.run(os.getenv('DISCORD_TOKEN'))
